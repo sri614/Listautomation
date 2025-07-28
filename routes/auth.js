bfe513fa6db5cad49346b357504deea5c5354768
sri614 <srikumaran@blueoshan.com>
2025-07-28 12:33:38 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-  res.json({"status":"done"})
-})
 
 // Login page
 router.get('/login', (req, res) => {
