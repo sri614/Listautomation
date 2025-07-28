d7df6e451bc97ebf638b3902ee5301071ce08423
sri <srikumaran@blueoshan.com>
2025-07-28 12:25:54 +0530

    live-update

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/',(req,res)=>{
+  res.json({"status":"done"})
+})
+
 // Login page
 router.get('/login', (req, res) => {
   res.render('login', {
